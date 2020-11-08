06ec5d5bcc1dec6b071523c9d9378d74c730a01f
DShaulov <shaulovl55@gmail.com>
2020-11-08 22:28:40 +0200

    Final

---
+++ test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+enum Days{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wendesday,
+    Thursday,
+    Friday,
+    Saturday
+};
+
+int main(void){
+    int x = 5;
+    int y = 6;
+    if(Sunday == 0){
+        for(int i = 0; i <= 10; i++){
+            printf("WTF");
+        }
+    }
+}   
